712fd5d51bf140afbc49e7c931edf43139f1adda
Kabanova Karina <kabanova.karina@mail.ru>
2023-08-23 10:44:44 -0300

    chacnge styles

--- src/app/(main)/dish/[slug]/page.tsx
+++ src/app/(main)/dish/[slug]/page.tsx
@@ -4,7 +4,18 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <Link href="/">Home</Link>
+      <h1>Welcome to the custom page</h1>
+      <Link
+        href="/"
+        style={{
+          color: "blue",
+          fontSize: "2rem",
+          textDecoration: "underline",
+          cursor: "pointer",
+        }}
+      >
+        Link to home
+      </Link>
     </div>
   );
 };
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,8 +5,21 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <Link href="dish/123">Dish 123</Link>
-			<Image src="/next.svg" width={100} height={100} alt="next"></Image>
+			<h1>
+				Welcome to home page
+			</h1>
+      <Link
+        href="dish/123"
+        style={{
+          color: "blue",
+          fontSize: "2rem",
+          textDecoration: "underline",
+          cursor: "pointer",
+        }}
+      >
+        Link to another page
+      </Link>
+      <Image src="/next.svg" width={100} height={100} alt="next"></Image>
     </div>
   );
 };
